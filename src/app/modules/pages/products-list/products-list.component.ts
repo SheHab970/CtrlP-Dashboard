b1c5c78e60435a356419351ce868503aa9ddc25e
Esraa nashaat <esraanashaat07@gmail.com>
2024-11-17 19:12:37 +0200

    third

--- src/app/modules/pages/products-list/products-list.component.ts
+++ src/app/modules/pages/products-list/products-list.component.ts
@@ -1,12 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { RatingModule } from 'primeng/rating';
+import { TagModule } from 'primeng/tag';
+import {
+  ProductService,
+  Product,
+} from '../../../core/services/product.service';
 
 @Component({
-  selector: 'app-products-list',
+  selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    TableModule,
+    ButtonModule,
+    RatingModule,
+    TagModule,
+  ],
   templateUrl: './products-list.component.html',
-  styleUrl: './products-list.component.scss'
+  styleUrls: ['./products-list.component.scss'],
 })
-export class ProductsListComponent {
+export class ProductsListComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    // Fetch products from the service
+    this.loadProducts();
+  }
+
+  loadProducts() {
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data;
+      console.log(this.products);
+    });
+  }
+
+  getSeverity(
+    status: string
+  ):
+    | 'success'
+    | 'secondary'
+    | 'info'
+    | 'warning'
+    | 'danger'
+    | 'contrast'
+    | undefined {
+    switch (status) {
+      case 'INSTOCK':
+        return 'success';
+      case 'LOWSTOCK':
+        return 'warning';
+      case 'OUTOFSTOCK':
+        return 'danger';
+      default:
+        return undefined;
+    }
+  }
+
+  refreshProducts() {
+    this.loadProducts();
+  }
 }
