14c33fcbbeee58a68ef40f29b167757ca453beac
SheHab970 <shehab202205097@gmail.com>
2024-12-02 20:05:52 +0200

    Add Orders page and Order Details page

--- src/app/modules/pages/orders-list/orders-list.component.ts
+++ src/app/modules/pages/orders-list/orders-list.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { FormsModule } from '@angular/forms';
+import { SearchComponent } from '../../../share/componrnts/search/search.component';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-orders-list',
   standalone: true,
-  imports: [],
+  imports: [
+    SearchComponent,
+    ButtonModule,
+    TableModule,
+    TagModule,
+    FormsModule,
+    RouterLink,
+    RouterModule],
   templateUrl: './orders-list.component.html',
   styleUrl: './orders-list.component.scss'
 })
 export class OrdersListComponent {
-
+  products: any[] = [
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+  ];
+  
 }
